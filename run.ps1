81ad4865d5241d2293878a700dc31f6f084936c8
0SU2 <or_zv18v@outlook.com>
2025-02-24 05:50:47 -0600

    Making the steps to install python 3.11 inside this proyect

--- run.ps1
+++ run.ps1
@@ -1,3 +1,11 @@
+Write-Output "This script need a lower version of python 3.12<= to work..."
+Start-Sleep -Seconds 3
+Write-Output "Please install python inside this folder with the name 'python-3.11', in the setup it will ask you if you want to make a CUSTOM INSTALLATION, you select that option, click NEXT, and install inside of this folder with the previous name 'python-3.11'."
+Write-Output "You need to use this link -> https://www.python.org/ftp/python/3.11.1/python-3.11.1-amd64.exe,"
+Write-Output "copy and pasted, or shift + right click to open in your browser, and follow the previous steps, then return to this script and continue."
+# Wait to user to install a python version inside the folder
+PAUSE
+
 $currenDirectoryInstall = Get-Location
 # check if python was previously install
 if (-not(Test-Path -path $currenDirectoryInstall'\python-custom-3.9')) {
