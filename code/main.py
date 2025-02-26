6c3a051205892a11a0d18f7348c217bcd1dfa641
0SU2 <or_zv18v@outlook.com>
2025-02-25 20:52:56 -0600

    Making the first test with the script

--- code/main.py
+++ code/main.py
@@ -9,12 +9,12 @@ from visualization import visualize
 
 # TO-DO
 # Have multiple files and iterate through them
-IMAGE_FILE = '..\assets\image.jpg'
+IMAGE_FILE = '../assets/image.jpg'
 img = cv2.imread(IMAGE_FILE)
-cv2.imshow(img)
+cv2.imshow("test",img)
 
 # STEP 2: Create an FaceDetector object.
-base_options = python.BaseOptions(model_asset_path='detector.tflite')
+base_options = python.BaseOptions(model_asset_path='../detector.tflite')
 options = vision.FaceDetectorOptions(base_options=base_options)
 detector = vision.FaceDetector.create_from_options(options)
 
@@ -28,4 +28,4 @@ detection_result = detector.detect(image)
 image_copy = np.copy(image.numpy_view())
 annotated_image = visualize(image_copy, detection_result)
 rgb_annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
-cv2.imshow(rgb_annotated_image)
+cv2.imshow("test",rgb_annotated_image)
